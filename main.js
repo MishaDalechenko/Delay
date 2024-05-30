7fc4a38792c36d9a324505daa6e921c5e532aee6
MishaDalechenko <mishenka@gmail.com>
2024-05-30 22:42:08 +0300

    init commit

---
+++ main.js
@@ -0,0 +1,27 @@
+import './casino.scss'
+
+
+// // Находим кнопку и модальное окно
+// const loginButton = document.getElementById("loginButton");
+
+
+// // При клике на кнопку открываем модальное окно
+// loginButton.addEventListener("click", function () {
+//     auth.style.display = "block";
+// });
+
+// // Находим элемент для закрытия окна
+// const closeBtn = loginModal.querySelector(".close");
+
+// // При клике на элемент закрытия скрываем модальное окно
+// closeBtn.addEventListener("click", function () {
+//     loginModal.style.display = "none";
+// });
+
+// // Закрытие модального окна при клике вне него
+// window.addEventListener("click", function (event) {
+//     if (event.target == loginModal) {
+//         loginModal.style.display = "none";
+//     }
+// });
+
